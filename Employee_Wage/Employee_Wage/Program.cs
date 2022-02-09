79384cb583ac6eb8f8091f37a9dcae5ea37cb6cf
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-09 14:27:28 +0530

    ADDED:Check Employee is Present or Absent

---
+++ Employee_Wage/Employee_Wage/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Employee_Wage;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Random random = new Random();
+        int empCheck = random.Next(0, 2);
+
+        int Absent = 0;
+        if (empCheck == Absent)
+        {
+            Console.WriteLine("Employee is Absent");
+        }
+        else
+        {
+            Console.WriteLine("Employee is Present");
+        }
+    }
+}
