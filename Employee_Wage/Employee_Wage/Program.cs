f4534cc140303bb0a9c467d2d6e307a2cb02b09f
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-09 14:39:17 +0530

    ADDED:Calculate Dialy Employee Wage

--- Employee_Wage/Employee_Wage/Program.cs
+++ Employee_Wage/Employee_Wage/Program.cs
@@ -9,15 +9,21 @@ class Program
     {
         Random random = new Random();
         int empCheck = random.Next(0, 2);
-
+        int WorkingHr = 0;
+        int WagePerHrs = 20;
         int Absent = 0;
         if (empCheck == Absent)
         {
+            WorkingHr = 0;
             Console.WriteLine("Employee is Absent");
         }
         else
         {
+            WorkingHr = 8;
             Console.WriteLine("Employee is Present");
         }
+
+        int EmpDailyWage = WagePerHrs * WorkingHr;
+        Console.WriteLine($"Employee Daily Wage is {EmpDailyWage}");
     }
 }
