f12ffc2d84a5c78a25ea6917e559e634ab390dfe
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-10 14:26:58 +0530

    ADDED:Proper indentation

--- Employee_Wage/Employee_Wage/Program.cs
+++ Employee_Wage/Employee_Wage/Program.cs
@@ -11,9 +11,13 @@ class Program
     {
         Random random = new Random();
         int empCheck = random.Next(0, 3);
+        //Veriables
         int WorkingHr = 0;
+
+        //Constants
         const int ABSENT = 0;
         const int FULLTIME = 1;
+
         switch (empCheck)
         {
             case ABSENT:
@@ -38,22 +42,26 @@ class Program
     }
     public static void Main(string[] args)
     {
+        //Constants
         int MAXDAY = 20;
         int TOTALWORKINGHRS = 100;
         int WAGEPERHRS = 20;
 
+        //Veriables
         int day = 0;
         int totalWorkingHrs = 0;
         int empTotalWage = 0;
-        Dictionary<int,int>dailywage = new Dictionary<int, int>();
+
+        Dictionary<int, int> dailywage = new Dictionary<int, int>();
+
         while (day < MAXDAY && totalWorkingHrs < TOTALWORKINGHRS)
         {
 
             totalWorkingHrs = totalWorkingHrs + calculate_working_hrs();
             int empDailyWage = WAGEPERHRS * calculate_working_hrs();
             empTotalWage = empTotalWage + empDailyWage;
-            dailywage[day]= empDailyWage;
-            Console.WriteLine( $"DAY {day+1} Wage is {dailywage[day]}");
+            dailywage[day] = empDailyWage;
+            Console.WriteLine($"DAY {day + 1} Wage is {dailywage[day]}");
             day++;
         }
         Console.WriteLine($"Total Days:{day} , Total Working Hours:{totalWorkingHrs} & Total Employee Wage:{empTotalWage}");
