6cef55a2984d0e59992a73f68f113be43edd5c66
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-10 12:47:08 +0530

    ADDED: Refactor the code to write a function to get work hours

--- Employee_Wage/Employee_Wage/Program.cs
+++ Employee_Wage/Employee_Wage/Program.cs
@@ -5,43 +5,59 @@ namespace Employee_Wage;
 
 class Program
 {
+    static int calculate_working_hrs()
+
+    {
+        Random random = new Random();
+        int empCheck = random.Next(0, 3);
+        int WorkingHr = 0;
+        const int ABSENT = 0;
+        const int FULLTIME = 1;
+        switch (empCheck)
+        {
+            case ABSENT:
+                WorkingHr = 0;
+                // Console.WriteLine("Employee is Absent");return WorkingHr;
+                return WorkingHr;
+                break;
+
+            case FULLTIME:
+                WorkingHr = 8;
+                // Console.WriteLine("Employee is Full Time Present");
+                return WorkingHr;
+                break;
+
+            default:
+                WorkingHr = 4;
+                //Console.WriteLine("Employee is Part Time Present");
+                return WorkingHr;
+                break;
+
+        }
+
+    }
     public static void Main(string[] args)
     {
         int MAXDAY = 20;
         int TOTALWORKINGHRS = 100;
+        int WAGEPERHRS = 20;
+
         int day = 0;
-        int workingHrs = 0;
-        while (day < MAXDAY && workingHrs < TOTALWORKINGHRS)
+        int totalworkingHrs = 0;
+        while (day < MAXDAY && totalworkingHrs < TOTALWORKINGHRS)
         {
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            int WorkingHr = 0;
-            int WagePerHrs = 20;
-            const int Absent = 0;
-            const int FullTime = 1;
-            switch (empCheck)
-            {
-                case Absent:
-                    WorkingHr = 0;
-                    Console.WriteLine("Employee is Absent");
-                    break;
 
-                case FullTime:
-                    WorkingHr = 8;
-                    Console.WriteLine("Employee is Full Time Present");
-                    break;
-
-                default:
-                    WorkingHr = 4;
-                    Console.WriteLine("Employee is Part Time Present");
-                    break;
-            }
-            workingHrs = workingHrs + WorkingHr;
-            int EmpDailyWage = WagePerHrs * WorkingHr;
-            Console.WriteLine($"Employee Daily Wage is {EmpDailyWage}");
+            totalworkingHrs = totalworkingHrs + calculate_working_hrs();
+            int empDailyWage = WAGEPERHRS * calculate_working_hrs();
+            Console.WriteLine($"Employee Daily Wage is {empDailyWage}");
             day++;
         }
-        Console.WriteLine($"Total Days:{day} & Total Working Hours:{workingHrs}");
-        
+        Console.WriteLine($"Total Days:{day} & Total Working Hours:{totalworkingHrs}");
+
     }
+
+
 }
+
+
+
