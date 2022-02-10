6090f8c8b741af90f1b86381bc9d2c181590ba88
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-10 14:11:25 +0530

    ADDED: Store the day and daily wages along with the total wage

--- Employee_Wage/Employee_Wage/Program.cs
+++ Employee_Wage/Employee_Wage/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Collections.Generic;
 
 namespace Employee_Wage;
 
@@ -44,15 +45,15 @@ class Program
         int day = 0;
         int totalWorkingHrs = 0;
         int empTotalWage = 0;
-        int[] dialywage = new int[MAXDAY];
+        Dictionary<int,int>dailywage = new Dictionary<int, int>();
         while (day < MAXDAY && totalWorkingHrs < TOTALWORKINGHRS)
         {
 
             totalWorkingHrs = totalWorkingHrs + calculate_working_hrs();
             int empDailyWage = WAGEPERHRS * calculate_working_hrs();
             empTotalWage = empTotalWage + empDailyWage;
-            dialywage[day] = empDailyWage;
-            Console.WriteLine( $"DAY {day+1} Wage is {dialywage[day]}");
+            dailywage[day]= empDailyWage;
+            Console.WriteLine( $"DAY {day+1} Wage is {dailywage[day]}");
             day++;
         }
         Console.WriteLine($"Total Days:{day} , Total Working Hours:{totalWorkingHrs} & Total Employee Wage:{empTotalWage}");
